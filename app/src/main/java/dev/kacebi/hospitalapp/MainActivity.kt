9bfbe7dfea70c83b0cb9698f99d1f47715302f02
Luka Parchukidze <luka.parchukidze.1@btu.edu.ge>
2020-06-30 14:58:07 +0400

    Commit Test

--- app/src/main/java/dev/kacebi/hospitalapp/MainActivity.kt
+++ app/src/main/java/dev/kacebi/hospitalapp/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Georgia", Toast.LENGTH_LONG).show()
     }
 }
